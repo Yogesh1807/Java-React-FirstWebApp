1472dd751b4d1785edd06e6a4020de156d1e716c
Yogesh1807 <satale.yogesh88@gmail.com>
2022-06-19 15:38:34 +0530

    initial commit

---
+++ src/main/java/com/firstapp/javareactwebapp/JavaReactWebappApplication.java
@@ -0,0 +1,13 @@
+package com.firstapp.javareactwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstapp/javareactwebapp/JavaReactWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapp.javareactwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
